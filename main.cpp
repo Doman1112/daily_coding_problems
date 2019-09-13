d05249e34aba0d4510d9f537e3d2855b4c48c709
Krzysztof Domanski <krzdom@googlemail.com>
2019-09-13 20:36:02 +0100

    Updated for some comments

--- main.cpp
+++ main.cpp
@@ -3,7 +3,15 @@
 #include <vector>
 #include <set>
 
-bool containsIntegersToAddUp(std::vector<int> listOfInts, int k) {
+/**
+ * Given a list of numbers and a number \p k, returns whether any 
+ * two numbers from the list add up to \p k.
+ * 
+ * @param[in] listOfInts    List of numbers.
+ * @param[in] k             Number to test.
+ * @return true if we found two numbers that add up to \p - false otherwise
+ */
+bool containsIntegersToAddUp(const std::vector<int>& listOfInts, const int k) {
 
     std::sort(listOfInts.begin(), listOfInts.end());
     std::set<int> secondTerms;
@@ -21,7 +29,8 @@ bool containsIntegersToAddUp(std::vector<int> listOfInts, int k) {
 }
 
 int main() {
-    
+    // Temporary dummy testing in main.
+    // TODO Add googletest    
     std::vector<int> v { 12, 4, 10, 7, 11};
 
     std::cout << "Contains: " << containsIntegersToAddUp(v, 18) << "\n";
