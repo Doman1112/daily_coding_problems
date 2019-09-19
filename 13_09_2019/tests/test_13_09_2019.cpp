a42b374216367076820b9fdbe9dde78ae8dde242
Doman1112 <krzdom@googlemail.com>
2019-09-19 12:19:23 +0100

    Update test_13_09_2019.cpp
    Update test to check something

--- 13_09_2019/tests/test_13_09_2019.cpp
+++ 13_09_2019/tests/test_13_09_2019.cpp
@@ -3,11 +3,12 @@
 #include "gtest/gtest.h"
 #include "code_solution.h"
 
-TEST(code_solution_1, code_solution_1) {
+TEST(code_solution_1, small_list) {
+    std::vector<int> listOfNumbers = { 1, 2, 3, 4 };
+    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 7), false);
+}
+
+TEST(code_solution_1, small_list_2) {
     std::vector<int> listOfNumbers = { 1, 2, 3, 4 };
     EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 7), true);
-
-    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 8), false);
-
-    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 0), false);
 }
