c92b370b79988c692a0d80507350e9f6c7797f0c
Doman1112 <krzdom@googlemail.com>
2019-09-19 13:14:16 +0100

    Update test_13_09_2019.cpp

--- 13_09_2019/tests/test_13_09_2019.cpp
+++ 13_09_2019/tests/test_13_09_2019.cpp
@@ -5,10 +5,10 @@
 
 TEST(code_solution_1, small_list) {
     std::vector<int> listOfNumbers = { 1, 2, 3, 4 };
-    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 7), false);
+    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 7), true);
 }
 
 TEST(code_solution_1, small_list_2) {
     std::vector<int> listOfNumbers = { 1, 2, 3, 4 };
-    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 7), true);
+    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 8), false);
 }
