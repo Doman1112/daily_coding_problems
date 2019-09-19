9ebaabf4c8a0ce70b11f96eca785feed70147d38
Doman1112 <krzdom@googlemail.com>
2019-09-19 15:15:52 +0100

    Update test_13_09_2019.cpp

--- 13_09_2019/tests/test_13_09_2019.cpp
+++ 13_09_2019/tests/test_13_09_2019.cpp
@@ -1,17 +1,15 @@
 #include <vector>
 
 #include "gtest/gtest.h"
-// #include "code_solution.h"
+#include "code_solution.h"
 
 TEST(code_solution_1, small_list) {
-    //std::vector<int> listOfNumbers = { 1, 2, 3, 4 };
-    //EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 7), true);
-    EXPECT_EQ(true, true) << "Hopefully passed";
+    std::vector<int> listOfNumbers = { 1, 2, 3, 4 };
+    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 7), true);
     
 }
 
 TEST(code_solution_1, small_list_2) {
-    //std::vector<int> listOfNumbers = { 1, 2, 3, 4 };
-    //EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 8), false);
-    EXPECT_EQ(true, true) << "Hopefully passed";
+    std::vector<int> listOfNumbers = { 1, 2, 3, 4 };
+    EXPECT_EQ(containsIntegersToAddUp(listOfNumbers, 8), false);
 }
